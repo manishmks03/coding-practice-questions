661001c37346cc001ecadf767b90ed1919187d48
Manish Singh <manishkumarsingh@airasia.com>
2020-12-03 22:44:18 +0530

    longest substr without repeating char

---
+++ Longest_Substring_Without_Repeating_Characters.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  if (!s) {
+    return 0;
+  }
+  if (s.length === 1) {
+    return 1;
+  }
+  let res = {
+    test: {},
+    str: "",
+    length: 0
+  };
+  let len1 = 0;
+  let str1 = "";
+  for (let i = 0; i < s.length - 1; i++) {
+    let len = 1;
+    let str = s[i];
+    let test = {
+      [s[i]]: 1
+    };
+    for (let j = i + 1; j < s.length; j++) {
+      if (!test[s[j]]) {
+        len += 1;
+        str = str + s[j];
+        test[s[j]] = 1;
+      } else {
+        break;
+      }
+    }
+    if (len1 < len) {
+      len1 = len;
+      str1 = str;
+    }
+  }
+  return len1;
+};
+
+lengthOfLongestSubstring("pwwkeaswwpoiuytre");

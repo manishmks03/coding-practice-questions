6f12513a4a14078dfcc9fe5be6879a07fb81d0af
Manish Singh <manishkumarsingh@airasia.com>
2020-12-08 18:52:56 +0530

    balance parantehesis

---
+++ balanced-paranthesis.js
@@ -0,0 +1,21 @@
+/* 
+Your previous Plain Text content is preserved below:
+
+/*
+Given a string with alpha-numeric characters and parentheses, return a string with balanced parentheses by removing the fewest characters possible. You cannot add anything to the string.
+
+balance("()") -> "()"
+balance("a(b)c)") -> "a(b)c"
+balance("ab)") -> "ab"
+balance(")(") -> ""
+balance("(((((") -> ""
+balance("(()()(") -> "()()"
+balance(")((a(b)))(") -> "((a(b)))"
+*/
+
+// TODO
+
+function balance(text) {}
+
+const text = "a(b)cde)";
+balance(text);

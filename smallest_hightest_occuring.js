15b06a3d84ca489ae3ba1bdb8905b451a39bcb52
Manish Singh <manishkumarsingh@airasia.com>
2020-12-09 11:06:00 +0530

    smallest highest occuring

---
+++ smallest_hightest_occuring.js
@@ -0,0 +1,27 @@
+function mostFrequenMeal(array) {
+  const obj = {};
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    if (!obj[element]) {
+      obj[element] = 1;
+    } else {
+      obj[element] = obj[element] + 1;
+    }
+  }
+  const maxCount = Math.max(...Object.values(obj));
+  let smallest = null;
+  Object.keys(obj).map(key => {
+    if (obj[key] === maxCount) {
+      if (!smallest) {
+        smallest = key;
+      } else if (smallest > key) {
+        smallest = key;
+      }
+    }
+  });
+  console.log("Smallest : ", smallest);
+  return smallest;
+}
+
+const array = [1, 4, 4, 4, 3, 2, 1, 6];
+console.log(mostFrequenMeal(array));

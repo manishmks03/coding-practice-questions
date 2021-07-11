10628979e589c19a0cf0e76fbbf5ba36a5e42e02
Manish Singh <manishkumarsingh@airasia.com>
2021-07-11 17:54:34 +0530

    leetcode problem 1

---
+++ leetcode/1_two_sum.js
@@ -0,0 +1,59 @@
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// Using brute force - nested loops O(n2)
+// var twoSum = function (nums, target) {
+//   if (nums.length < 2 || nums.length > Math.pow(10, 4)) return;
+
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   return;
+// };
+
+// Using hashmap - O(n)
+var twoSum = function (nums, target) {
+  if (
+    nums.length < 2 ||
+    nums.length > Math.pow(10, 4) ||
+    target > Math.pow(10, 9)
+  )
+    return;
+
+  const hashMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const secondNum = target - num;
+    if (hashMap.hasOwnProperty(secondNum) && hashMap[secondNum] !== i) {
+      return [hashMap[secondNum], i];
+    }
+    hashMap[num] = i;
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
+console.log(twoSum([2, 5, 5, 11], 10));
